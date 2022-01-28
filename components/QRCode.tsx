802baa71f7281316b913909c9786f4c6ace015d4
caiprozect <caiprozect@gmail.com>
2022-01-28 15:57:54 +0900

    shrink for small phones

--- components/QRCode.tsx
+++ components/QRCode.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 function QRCode() {
   return (
     <div className="mx-auto w-auto flex-col items-center rounded-3xl bg-white p-5 shadow-md">
-      <div className="relative mx-auto mb-6 h-80 w-80">
+      <div className="relative mx-auto mb-6 h-64 w-64 sm:h-80 sm:w-80">
         <Image
           className="rounded-3xl"
           src="/images/image-qr-code.png"
@@ -11,8 +11,8 @@ function QRCode() {
           objectFit="contain"
         />
       </div>
-      <div className="flex w-80 flex-col font-outfit">
-        <h1 className="mb-3 p-2 text-center text-2xl font-bold text-[#1f3251]">
+      <div className="flex w-64 flex-col items-center font-outfit sm:w-80">
+        <h1 className="mb-4 px-2 text-center text-2xl font-bold text-[#1f3251]">
           Improve your front-end skills by building projects
         </h1>
         <p className="px-2 pb-8 text-center text-lg text-[#7b879d]">
